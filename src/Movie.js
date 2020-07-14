f8cde094c4a0401b6d07ffd602a0a3fa0af06c46
yeon.hoo <gnfka0828@gmail.com>
2020-07-14 11:26:50 +0900

    feat: Add Genres and Styling the Movies
    feat: Add Genres and Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css"
 
 // function component의 인자로 props 말고 props의 특정 값만 가져올 때는 꼭 curly bracket으로 감싸주세요
 // ex)
 // function Movies( id, year, title, summary, poster) { ... } // X
 // function Movies( {id, year, title, summary, poster}) {...} // O
 
-function Movie({id, year, title, summary, poster}) {
-    console.log(title);
-    return <h4>title : {title}</h4>
+function Movie({id, year, title, summary, poster, genres}) {
+    return (
+        <div className="movie">  
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">
+                {genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                    {genre}
+                </li>
+                ))}
+            </ul>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+        </div>
+    )
 }
 
 Movie.prototype = {
@@ -16,7 +32,8 @@ Movie.prototype = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
